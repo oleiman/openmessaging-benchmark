65d1b97cb1cccbbff7cceb7975057f57d41418d3
Oren Leiman <oren.leiman@redpanda.com>
2024-07-22 13:02:52 -0700

    cruft

--- wasm-data-transform/redact-js/src/index.ts
+++ wasm-data-transform/redact-js/src/index.ts
@@ -3,37 +3,53 @@ import { createHash } from "crypto";
 
 
 // maybe not needed
-const schema = {
-    "type": "record",
-    "name":"Interop",
-    "namespace": "org.apache.avro",
-    "fields": [
-        {"name": "intField", "type": "int"},
-        {"name": "longField", "type": "long"},
-        {"name": "stringField", "type": "string"},
-        {"name": "boolField", "type": "boolean"},
-        {"name": "floatField", "type": "float"},
-        {"name": "doubleField", "type": "double"},
-        {"name": "bytesField", "type": "bytes"},
-        {"name": "nullField", "type": "null"},
-        {"name": "arrayField", "type": {"type": "array", "items": "double"}},
-        {"name": "mapField", "type":
-         {"type": "map", "values":
-          {"type": "record", "name": "Foo",
-           "fields": [{"name": "label", "type": "string"}]}}},
-        {"name": "unionField", "type":
-         ["boolean", "double", {"type": "array", "items": "bytes"}]},
-        {"name": "enumField", "type":
-         {"type": "enum", "name": "Kind", "symbols": ["A","B","C"]}},
-        {"name": "fixedField", "type":
-         {"type": "fixed", "name": "MD5", "size": 16}},
-        {"name": "recordField", "type":
-         {"type": "record", "name": "Node",
-          "fields": [
-              {"name": "label", "type": "string"},
-              {"name": "children", "type": {"type": "array", "items": "Node"}}]}}
-    ]
-};
+// const schema = {
+//     "type": "record",
+//     "name": "Interop",
+//     "namespace": "org.apache.avro",
+//     "fields": [
+//         { "name": "intField", "type": "int" },
+//         { "name": "longField", "type": "long" },
+//         { "name": "stringField", "type": "string" },
+//         { "name": "boolField", "type": "boolean" },
+//         { "name": "floatField", "type": "float" },
+//         { "name": "doubleField", "type": "double" },
+//         { "name": "bytesField", "type": "bytes" },
+//         { "name": "nullField", "type": "null" },
+//         { "name": "arrayField", "type": { "type": "array", "items": "double" } },
+//         {
+//             "name": "mapField", "type":
+//             {
+//                 "type": "map", "values":
+//                 {
+//                     "type": "record", "name": "Foo",
+//                     "fields": [{ "name": "label", "type": "string" }]
+//                 }
+//             }
+//         },
+//         {
+//             "name": "unionField", "type":
+//                 ["boolean", "double", { "type": "array", "items": "bytes" }]
+//         },
+//         {
+//             "name": "enumField", "type":
+//                 { "type": "enum", "name": "Kind", "symbols": ["A", "B", "C"] }
+//         },
+//         {
+//             "name": "fixedField", "type":
+//                 { "type": "fixed", "name": "MD5", "size": 16 }
+//         },
+//         {
+//             "name": "recordField", "type":
+//             {
+//                 "type": "record", "name": "Node",
+//                 "fields": [
+//                     { "name": "label", "type": "string" },
+//                     { "name": "children", "type": { "type": "array", "items": "Node" } }]
+//             }
+//         }
+//     ]
+// };
 
 
 function i32Hash(i32) {
@@ -79,7 +95,6 @@ function redact(o) {
 }
 
 onRecordWritten((event: OnRecordWrittenEvent, writer: RecordWriter) => {
-    console.warn(event.record.value.text());
     var val = event.record.value.json();
     val = redact(val);
     writer.write({
